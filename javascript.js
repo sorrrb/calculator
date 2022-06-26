8d83647338d0ecfddf9cbed285e8f358ffc7dec1
sorrrb <cdrukepike@gmail.com>
2022-06-26 14:00:35 -0400

    Refactor code logic for reusability

--- javascript.js
+++ javascript.js
@@ -1,5 +1,47 @@
-let displayApp = () => {
-  const CALC_CONTAINER = document.querySelector('div.wrapper');
+let display = () => {
+  const container = document.querySelector('div.calculator');
+  
+  const ROWS = 6;
+  const COLS = 4;
+
+  // CALCULATOR BUTTON VALUES - array of strings denoting button functions
+  const BUTTON_VALUES = ['C', '±', '%', '÷', '7', '8', '9', 'x', '4', '5', '6',
+       '-', '1', '2', '3', '+', '0', '.', '='];
+  
+  //DEFINE BUTTONS - nested for loop to create buttons
+  for (let i = 0; i < ROWS; i++) {
+    for (let j = 0; j < COLS; j++) {
+      if (i === 0) {
+        const NUM_DISPLAY = document.createElement('div');
+        NUM_DISPLAY.classList.add('display');
+        container.appendChild(NUM_DISPLAY);
+        break;
+      }
+      else if (i === 5 && j < 1) {
+        const SPAN_TWO = document.createElement('div');
+        SPAN_TWO.classList.add('click');
+        SPAN_TWO.classList.add('zero-btn')
+        SPAN_TWO.textContent = 0;
+        container.appendChild(SPAN_TWO);
+        j++;
+      }
+      else {
+        const CALC_SQUARE = document.createElement('div');
+        CALC_SQUARE.classList.add('click');
+        container.appendChild(CALC_SQUARE);
+      }
+    }
+  }
+
+  const DISPLAY = document.querySelector('div.display');
+  DISPLAY.textContent = 0;
+
+  let eachElement = DISPLAY.nextElementSibling;
+  for (let i = 0; i < BUTTON_VALUES.length; i++) {
+    eachElement.textContent = BUTTON_VALUES[i];
+    eachElement = eachElement.nextElementSibling;
+  }
+  console.log(DISPLAY);
   
 }
 
@@ -11,18 +53,22 @@ let multiply = (a,b) => a * b;
 let divide = (a,b) => a / b;
 
 let operate = (operator, x, y) => {
+  const plus = 'add';
+  const minus = 'subtract';
+  const times = 'multiply';
+  const over = 'divide';
   let result;
   switch(operator) {
-    case 'add':
+    case plus:
       result = add(x,y);
       break;
-    case 'subtract':
+    case minus:
       result = subtract(x,y);
       break;
-    case 'multiply':
+    case times:
       result = multiply(x,y);
       break;
-    case 'divide':
+    case over:
       result = divide(x,y);
       break;
   }
@@ -33,10 +79,10 @@ console.log(add(10,20)); // 30
 console.log(subtract(55,12)); // 43
 console.log(multiply(7,12)); // 84
 console.log(divide(2,1)); // 2 
-
+console.log('BREAK POINT')
 console.log(operate('add',10,20));
 console.log(operate('subtract',55,12));
 console.log(operate('multiply',7,12));
 console.log(operate('divide',2,1));
 
-displayApp();
+display();
