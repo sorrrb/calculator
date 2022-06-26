9c890805ce4624e327c01e955658bdc6e7ed51a0
sorrrb <cdrukepike@gmail.com>
2022-06-26 15:48:20 -0400

    Implement basic button number functionality

--- javascript.js
+++ javascript.js
@@ -3,12 +3,14 @@ let display = () => {
   
   const ROWS = 6;
   const COLS = 4;
+  
+  let displayValue = 0;
 
   // CALCULATOR BUTTON VALUES - array of strings denoting button functions
   const BUTTON_VALUES = ['C', '±', '%', '÷', '7', '8', '9', 'x', '4', '5', '6',
        '-', '1', '2', '3', '+', '0', '.', '='];
   
-  //DEFINE BUTTONS - nested for loop to create buttons
+  // DEFINE BUTTONS - nested for loop to create buttons
   for (let i = 0; i < ROWS; i++) {
     for (let j = 0; j < COLS; j++) {
       if (i === 0) {
@@ -17,17 +19,30 @@ let display = () => {
         container.appendChild(NUM_DISPLAY);
         break;
       }
+      else if (i === 1 || j === 3) {
+        const CALC_OPERATOR = document.createElement('div');
+        CALC_OPERATOR.classList.add('click');
+        CALC_OPERATOR.classList.add('operate');
+        container.appendChild(CALC_OPERATOR);
+      }
       else if (i === 5 && j < 1) {
         const SPAN_TWO = document.createElement('div');
         SPAN_TWO.classList.add('click');
-        SPAN_TWO.classList.add('zero-btn')
-        SPAN_TWO.textContent = 0;
+        SPAN_TWO.classList.add('zero-btn');
+        SPAN_TWO.textContent = displayValue;
         container.appendChild(SPAN_TWO);
         j++;
+      }
+      else if (i === 5 && j === 2) {
+        const CALC_FLOAT = document.createElement('div');
+        CALC_FLOAT.classList.add('click');
+        CALC_FLOAT.classList.add('decimal');
+        container.appendChild(CALC_FLOAT);
       }
       else {
         const CALC_SQUARE = document.createElement('div');
         CALC_SQUARE.classList.add('click');
+        CALC_SQUARE.classList.add('num');
         container.appendChild(CALC_SQUARE);
       }
     }
@@ -36,13 +51,32 @@ let display = () => {
   const DISPLAY = document.querySelector('div.display');
   DISPLAY.textContent = 0;
 
+  // IMPLEMENT TEXT - add relevant button text content for each button
   let eachElement = DISPLAY.nextElementSibling;
   for (let i = 0; i < BUTTON_VALUES.length; i++) {
     eachElement.textContent = BUTTON_VALUES[i];
     eachElement = eachElement.nextElementSibling;
   }
-  console.log(DISPLAY);
-  
+
+  const NUMS = document.querySelectorAll('div.num');
+  const ZERO_NUM = document.querySelector('div.zero-btn');
+
+  function sendToDisplay(value) {
+    let shownValue = DISPLAY.textContent;
+    shownValue += value;
+    displayValue = +shownValue;
+    DISPLAY.textContent = displayValue;
+  }
+
+  let updateNum = (e) => {
+    sendToDisplay(e.target.textContent);
+  }
+
+  NUMS.forEach((number) => {
+    number.addEventListener('click', updateNum);
+  });
+
+  ZERO_NUM.addEventListener('click', updateNum);
 }
 
 // HELPER FUNCTIONS
@@ -74,15 +108,5 @@ let operate = (operator, x, y) => {
   }
   return result;
 }
-
-console.log(add(10,20)); // 30
-console.log(subtract(55,12)); // 43
-console.log(multiply(7,12)); // 84
-console.log(divide(2,1)); // 2 
-console.log('BREAK POINT')
-console.log(operate('add',10,20));
-console.log(operate('subtract',55,12));
-console.log(operate('multiply',7,12));
-console.log(operate('divide',2,1));
 
 display();
