c6946ebceef4ae21fc7f48a1c47eeaf667855041
sorrrb <cdrukepike@gmail.com>
2022-06-28 19:34:22 -0400

    Implement basic logic changes surrounding typecast

--- javascript.js
+++ javascript.js
@@ -7,6 +7,43 @@ let display = () => {
   // CALCULATOR BUTTON VALUES - array of strings denoting button functions
   const BUTTON_VALUES = ['C', '±', '%', '÷', '7', '8', '9', 'x', '4', '5', '6',
        '-', '1', '2', '3', '+', '0', '.', '='];
+
+  // OPERATIONS
+  const PLUS = `+`;
+  const MINUS = `-`;
+  const TIMES = `x`;
+  const OVER = `÷`;
+  const CLEAR = `C`;
+  const INVERT = `±`;
+  const PERCENT = `%`;
+  const DECIMAL = `.`;
+  const RESULT = `=`;
+  
+  // HELPER FUNCTIONS (MATH OPERATORS)
+
+  let add = (a,b) => a + b;
+  let subtract = (a,b) => a - b;
+  let multiply = (a,b) => a * b;
+  let divide = (a,b) => a / b;
+
+  let operate = (operator, x, y) => {
+    let result;
+    switch(operator) {
+      case PLUS:
+        result = add(x,y);
+        break;
+      case MINUS:
+        result = subtract(x,y);
+        break;
+      case TIMES:
+        result = multiply(x,y);
+        break;
+      case OVER:
+        result = divide(x,y);
+        break;
+    }
+    return result;
+  }
   
   // DEFINE BUTTONS - nested for loop to create buttons
   for (let i = 0; i < ROWS; i++) {
@@ -55,7 +92,6 @@ let display = () => {
 
   const DISPLAY = document.querySelector('div.display');
   
-
   // IMPLEMENT TEXT - add relevant button text content for each button
   let eachElement = DISPLAY.nextElementSibling;
   for (let i = 0; i < BUTTON_VALUES.length; i++) {
@@ -66,80 +102,80 @@ let display = () => {
   const NUMS = document.querySelectorAll('div.num');
   const ZERO_NUM = document.querySelector('div.zero-btn');
   const OPERATORS = document.querySelectorAll('div.function');
+  const FLOAT = document.querySelector('div.decimal');
 
-  let callAlert = () => alert(`hi!`);
+  let displayValue = 0;
+  let displayString = '';
+  DISPLAY.textContent = displayValue;
+  let storedOperand = 0;
+  let storedOperator = '';
+  let currentCalcDisplay;
 
-  let calculateDisplay = () => {
-    
+  let calculateDisplay = (e) => {
+    let pressedButton = e.target.textContent;
+    let castTypeButton = Number(pressedButton);
+    let isNumberBool = (isNaN(castTypeButton) ? true : false);
+    if (isNumberBool) {
+      switch (e.target.textContent) {
+        case CLEAR:
+          displayValue = 0;
+          displayString = '';
+          currentCalcDisplay = displayValue
+          storedOperator = displayString;
+          storedOperand = displayValue;
+          DISPLAY.textContent = displayValue;
+          break;
+        case INVERT:
+          displayValue = -displayValue;
+          DISPLAY.textContent = displayValue;
+          break;
+        case PERCENT:
+          break;
+        case RESULT:
+          break;
+        case PLUS:
+          if (storedOperator === '') {
+            storedOperator = PLUS;
+            storedOperand = displayValue;
+            displayValue = 0;
+            displayString = '';
+            break;
+          }
+          else {
+            currentCalcDisplay = displayValue;
+            displayValue = operate(storedOperator, storedOperand, currentCalcDisplay);
+            DISPLAY.textContent = displayValue;
+            break;
+          }
+      }
+    }
+    else {
+      let hasActiveOperation = (storedOperator === '' ? false : true);
+      if (hasActiveOperation && DISPLAY.textContent === '0') {
+        displayString = '';
+        DISPLAY.textContent = displayString;
+      }
+      let checkNumCount = (DISPLAY.textContent.length <= 14 ? true : false);
+      if (checkNumCount) {
+        displayString += pressedButton;
+        displayValue = Number(displayString);
+        DISPLAY.textContent = displayValue;
+      }
+    }
   }
 
   // Event listeners for buttons
   NUMS.forEach((number) => {
-    number.addEventListener('click', callAlert); // iterate through each number button
+    number.addEventListener('click', calculateDisplay); // iterate through each number button
   });
 
-  ZERO_NUM.addEventListener('click', callAlert); // add same listener to 0
+  ZERO_NUM.addEventListener('click', calculateDisplay); // add same listener to 0
 
   OPERATORS.forEach((button) => {
-    switch (button.textContent) {
-      case 'C':
-        button.addEventListener('click', callAlert);
-        break;
-      case '+':
-        button.addEventListener('click', calculateDisplay);
-        break;
-      case '-':
-        button.addEventListener('click', callAlert);
-        break;
-      case 'x':
-        button.addEventListener('click', callAlert);
-        break;
-      case '÷':
-        button.addEventListener('click', callAlert);
-        break;
-      case '±':
-        button.addEventListener('click', callAlert);
-        break;
-      case '%':
-        button.addEventListener('click', callAlert);
-        break;
-      case '=':
-        button.addEventListener('click', callAlert);
-        break;
-    } // DECIMAL OMITTED
+    button.addEventListener('click', calculateDisplay);
   });
+
+  FLOAT.addEventListener('click', calculateDisplay);
 }
-
-// HELPER FUNCTIONS
-
-let add = (a,b) => a + b;
-let subtract = (a,b) => a - b;
-let multiply = (a,b) => a * b;
-let divide = (a,b) => a / b;
-
-let operate = (operator, x, y) => {
-  let result;
-  switch(operator) {
-    case plus:
-      result = add(x,y);
-      break;
-    case minus:
-      result = subtract(x,y);
-      break;
-    case times:
-      result = multiply(x,y);
-      break;
-    case over:
-      result = divide(x,y);
-      break;
-  }
-  return result;
-}
-
-// OPERATIONS
-const plus = `+`;
-const minus = `-`;
-const times = `x`;
-const over = `÷`;
 
 display();
